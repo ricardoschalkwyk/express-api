69fcbff53ec91d3ecb2d12bc632f7d363ba593ac
Ricardo Schalkwyk <ricardo@loot.co.za>
2025-09-22 09:30:28 +0200

    chore: updated swagger ui server reference

--- src/swagger.ts
+++ src/swagger.ts
@@ -10,7 +10,7 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:3000", // Replace with your server URL
+        url: "https://express-api-qfyg.onrender.com/",
       },
     ],
   },
