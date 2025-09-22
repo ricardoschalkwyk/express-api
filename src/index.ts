45fd66ec5b287915f796846ff71ebe0f76ac8cfa
Ricardo Schalkwyk <ricardo@loot.co.za>
2025-09-22 09:26:23 +0200

    chore: deleted views engine

--- src/index.ts
+++ src/index.ts
@@ -16,10 +16,6 @@ dotenv.config();
 
 var app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(cors());
